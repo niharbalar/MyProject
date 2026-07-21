16bb814125ab83e8cec744cdb2eb0472f53d2ad5
Nihar Balar <niharbalar04@gmail.com>
2026-07-21 21:59:20 +0530

    Refactor attendance classes to improve employee attendance logging and reporting

--- Python/Object Oriented Programming/p13.py
+++ Python/Object Oriented Programming/p13.py
@@ -1,6 +1,7 @@
 from abc import ABC, abstractmethod
 from datetime import datetime
 
+
 class Attendance(ABC):
     def __init__(self, i, n, t):
         self.id = i
@@ -15,10 +16,11 @@ class Attendance(ABC):
     def do_attendance(self):
         pass
 
-class Office_Employee(Attendance):
-    def __init__(self, i, n, t):
-        super().__init__(i, n, t)
+    @abstractmethod
+    def count_attd(self):
+        pass
 
+class Office_Employee(Attendance):
     def do_attendance(self):
         date = datetime.now().strftime("%d-%m-%Y")
 
@@ -26,34 +28,60 @@ class Office_Employee(Attendance):
             file.write(f"{self.id},{date}\n")
 
     def count_attd(self):
-        employees = {}
         count = 0
-
         today = datetime.now().strftime("%d-%m-%Y")
 
-        with open("./Python/Object Oriented Programming/allEmp.txt", "r") as file:
-            for x in file:
-                name, emp_id, emp_type = x.strip().split(",")
-
-                employees[int(emp_id)] = {
-                    "name": name,
-                    "type": emp_type
-                }
-
         with open("./Python/Object Oriented Programming/officeAtt.txt", "r") as file:
+            for x in file:
+                emp_id, date = x.strip().split(",")
+
+                if date == today:
+                    print("ID:", emp_id, "Type: office", "Date:", date)
+                    count += 1
+
+        print("Total Office Attendance:", count)
+
+class Remote_Employee(Attendance):
+    def do_attendance(self):
+        date = datetime.now().strftime("%d-%m-%Y")
+
+        with open("./Python/Object Oriented Programming/remoteAtt.txt", "a") as file:
+            file.write(f"{self.id},{date}\n")
+
+    def count_attd(self):
+        count = 0
+        today = datetime.now().strftime("%d-%m-%Y")
+
+        with open("./Python/Object Oriented Programming/remoteAtt.txt", "r") as file:
             for y in file:
                 emp_id, date = y.strip().split(",")
 
                 if date == today:
-                    print(
-                        "ID:", emp_id,
-                        "Type:", employees[int(emp_id)]["type"],
-                        "Date:", date
-                    )
-
+                    print("ID:", emp_id, "Type: remote", "Date:", date)
                     count += 1
 
-        print("Total Employee Attendance:", count)
+        print("Total Remote Attendance:", count)
+
+class Intern_Employee(Attendance):
+    def do_attendance(self):
+        date = datetime.now().strftime("%d-%m-%Y")
+
+        with open("./Python/Object Oriented Programming/internAtt.txt", "a") as file:
+            file.write(f"{self.id},{date}\n")
+
+    def count_attd(self):
+        count = 0
+        today = datetime.now().strftime("%d-%m-%Y")
+
+        with open("./Python/Object Oriented Programming/internAtt.txt", "r") as file:
+            for z in file:
+                emp_id, date = z.strip().split(",")
+
+                if date == today:
+                    print("ID:", emp_id, "Type: intern", "Date:", date)
+                    count += 1
+
+        print("Total Intern Attendance:", count)
 
 
 while True:
@@ -61,15 +89,30 @@ while True:
     name = input("Enter your Name: ")
     type = input("Enter your employment type (office, remote, intern): ").lower()
 
-    o = Office_Employee(id, name, type)
-    o.addEmployee()
-
     if type == "office":
-        o.do_attendance()
+        employee = Office_Employee(id, name, type)
+
+    elif type == "remote":
+        employee = Remote_Employee(id, name, type)
+
+    elif type == "intern":
+        employee = Intern_Employee(id, name, type)
+
+    else:
+        print("Invalid employee type")
+        continue
+
+    employee.addEmployee()
+    employee.do_attendance()
 
     choice = input("\nDo you want to exit? (y/n): ")
 
     if choice.lower() == "y":
         break
 
-o.count_attd()
+
+print("\nAttendance Details")
+
+Office_Employee(0, "", "office").count_attd()
+Remote_Employee(0, "", "remote").count_attd()
+Intern_Employee(0, "", "intern").count_attd()
