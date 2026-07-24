3314ac92029aebb2ab0a22f17ebc56d721b28a78
Nihar Balar <niharbalar04@gmail.com>
2026-07-24 09:57:25 +0530

    Refactor exception handling in p1.py to consolidate error messages and improve clarity

--- Python/Exception Handling/p1.py
+++ Python/Exception Handling/p1.py
@@ -2,10 +2,16 @@ try:
     n1 = int(input("Enter number 1: "))
     n2 = int(input("Enter number 2: "))
     ans = n1/n2
-except ValueError:
-    print("Invalid Input")
-except ZeroDivisionError:
-    print("Division by Zero is NOT  Possible")
+# except ValueError:
+#     print("Invalid Input")
+# except ZeroDivisionError:
+#     print("Division by Zero is NOT  Possible")
+
+# except (ValueError, ZeroDivisionError):
+#     print("Invalid Input OR Division by Zero is NOT  Possible")
+
+except Exception as e:
+    print("Exception: ",e)
 else:
     print(f"Answer:{ans}")
 finally:
